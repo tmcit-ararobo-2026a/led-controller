b81881b3221e1b8fd99968243da1ae1cb29d9284
kanai0914 <ayuayugonbe@gmail.com>
2026-09-08 08:57:49 +0900

    chore:gn10_canとfdcanの対応

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -1,10 +1,17 @@
 #include "app/app.hpp"
 
+#include "app/led_info.hpp"
 #include "app/neopixel.hpp"
+#include "fdcan.h"
 #include "gn10_can/devices/led_server.hpp"
+#include "gn10_stm32_fdcan_driver/can_callback_helper.hpp"
+#include "gn10_stm32_fdcan_driver/fdcan_driver.hpp"
 #include "tim.h"
 
 Neopixel strip(&htim15, TIM_CHANNEL_1, 120);
+gn10_can::drivers::FDCANDriver fdcan1_driver(&hfdcan1);
+gn10_can::FDCANBus fdcan1_bus(fdcan1_driver);
+gn10_can::devices::LEDServer<LedInfo> led_server(fdcan1_bus, 2);
 
 void setup()
 {
@@ -41,5 +48,15 @@ extern "C" {
 void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef* htim)
 {
     strip.pulse_sent_callback(htim);
+}
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo0ITs)
+{
+    (void)RxFifo0ITs;
+    if (process_fdcan_fifo(hfdcan, &hfdcan1, fdcan1_bus, FDCAN_RX_FIFO0)) return;
+}
+void HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo1ITs)
+{
+    (void)RxFifo1ITs;
+    if (process_fdcan_fifo(hfdcan, &hfdcan1, fdcan1_bus, FDCAN_RX_FIFO0)) return;
 }
 }
