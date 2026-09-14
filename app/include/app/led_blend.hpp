f76b55c90fdc596f27a209dd62dcf1e28271d4f6
kni-0830 <ayuayugonbe@gmail.com>
2026-09-15 08:52:18 +0900

    refactor:名前変更

--- app/include/app/led_blend.hpp
+++ app/include/app/led_blend.hpp
@@ -8,21 +8,21 @@
 template <uint16_t PixelNum, class TargetLEDSets>
 std::array<Pixel, PixelNum> blend_pixels(const TargetLEDSets& targets)
 {
-    std::array<Pixel, PixelNum> result{};
+    std::array<Pixel, PixelNum> pixel{};
 
-    auto add_one = [&result](const std::array<Pixel, PixelNum>& src) {
+    auto add_one = [&pixel](const std::array<Pixel, PixelNum>& src) {
         for (uint16_t i = 0; i < PixelNum; ++i) {
             // int -> uint8にすることでオーバーフロー対策
-            int r       = static_cast<int>(result[i].r) + static_cast<int>(src[i].r);
-            int g       = static_cast<int>(result[i].g) + static_cast<int>(src[i].g);
-            int b       = static_cast<int>(result[i].b) + static_cast<int>(src[i].b);
-            result[i].r = static_cast<uint8_t>(std::clamp(r, 0, 255));
-            result[i].g = static_cast<uint8_t>(std::clamp(g, 0, 255));
-            result[i].b = static_cast<uint8_t>(std::clamp(b, 0, 255));
+            int r      = static_cast<int>(pixel[i].r) + static_cast<int>(src[i].r);
+            int g      = static_cast<int>(pixel[i].g) + static_cast<int>(src[i].g);
+            int b      = static_cast<int>(pixel[i].b) + static_cast<int>(src[i].b);
+            pixel[i].r = static_cast<uint8_t>(std::clamp(r, 0, 255));
+            pixel[i].g = static_cast<uint8_t>(std::clamp(g, 0, 255));
+            pixel[i].b = static_cast<uint8_t>(std::clamp(b, 0, 255));
         }
     };
 
     (add_one(targets.pixels()), ...);
 
-    return result;
+    return pixel;
 }
