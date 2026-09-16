3cb334051e6389135ad93a64d41d11cff8bec793
kanai0914 <ayuayugonbe@gmail.com>
2026-09-16 09:38:17 +0900

    feat:変換関数に新たな引数を追加

--- app/include/app/led_index_conversion.hpp
+++ app/include/app/led_index_conversion.hpp
@@ -9,7 +9,7 @@ class LEDIndexConversion
 public:
     LEDIndexConversion(float range_min, float range_max, uint16_t pixels_num);
 
-    uint8_t index_conversion(float value) const;
+    uint8_t index_conversion(float value, bool inversion = false) const;
 
 private:
     float range_min_;
--- app/src/led_index_conversion.cpp
+++ app/src/led_index_conversion.cpp
@@ -7,14 +7,19 @@ LEDIndexConversion::LEDIndexConversion(float range_min, float range_max, uint16_
     assert(pixels_num_ > 0 && "pixels_num must be greater than 0");
 }
 
-uint8_t LEDIndexConversion::index_conversion(float value) const
+uint8_t LEDIndexConversion::index_conversion(float value, bool inversion) const
 {
     value = std::clamp(value, range_min_, range_max_);
 
-    float range = range_max_ - range_min_;
-    float ratio = (value - range_min_) / range;
+    float range       = range_max_ - range_min_;
+    float ratio       = (value - range_min_) / range;
+    float index_float = ratio * (pixels_num_ - 1);
 
-    float pixel_f = ratio * (pixels_num_ - 1);
+    uint8_t index = static_cast<uint8_t>(std::round(index_float));
 
-    return static_cast<uint8_t>(std::round(pixel_f));
+    if (inversion) {
+        index = (pixels_num_ - 1) - index;
+    }
+
+    return index;
 }
