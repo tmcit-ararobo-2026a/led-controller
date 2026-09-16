6bea060d136777750491a15a83b7c2574ca377c0
kanai0914 <ayuayugonbe@gmail.com>
2026-09-16 14:51:54 +0900

    fix:まちがったCHANNELとhtimを正しくした

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -45,7 +45,7 @@ gn10_can::devices::LEDServer<LEDInformation> led_server_info(fdcan1_bus, 2);
 
 // neopixel
 Neopixel16bit<BEHIND_PIXEL_BREAK> behind(&htim15, TIM_CHANNEL_1);
-Neopixel32bit<BEHIND_PIXEL_SUM> belt_behind(&htim2, TIM_CHANNEL_4);
+Neopixel32bit<BEHIND_PIXEL_SUM> belt_behind(&htim15, TIM_CHANNEL_1);
 Neopixel32bit<FRONT_PIXEL_SUM> localization_front(&htim2, TIM_CHANNEL_1);
 
 void setup()
@@ -105,7 +105,7 @@ void loop()
         HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
     }
     // led_command.bucket1_angle_yaw_rad = 0.3f;
-    // control_led_localization(led_command);
+    control_led_localization(led_command);
 }
 
 extern "C" {
